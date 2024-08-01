3b73e1d5744b495fdb21d4b63c40bfcd16d45f02
unknown <ushka_ya@mail.ru>
2024-08-02 02:53:24 +0300

    added description block

--- src/components/mainContent/description/Description.tsx
+++ src/components/mainContent/description/Description.tsx
@@ -1,15 +1,18 @@
 import { AccordionUsageProps } from 'mainTypes'
 import * as React from 'react'
+import '../../../styles/description.scss'
 
 export function Description({ data }: AccordionUsageProps) {
   return (
     <section className={`grey__bgc`}>
-      <div className="block">
-        <div className="description">
+      <div className="block ">
+        <div className="description__block">
           {data.map((list, index) => (
             <React.Fragment key={index}>
-              <h3 className={`accordion-summary`}>{list.title}</h3>
-              <p className="description__text">{list.text}</p>
+              <div className="description">
+                <h3 className={`accordion-summary`}>{list.title}</h3>
+                <p className="description__text">{list.text}</p>
+              </div>
             </React.Fragment>
           ))}
         </div>
